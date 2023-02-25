475701067ca81df036b66f9943cd818cc6f6abe5
YashwinL <yashwinlakshmipathy@gmail.com>
2023-02-25 23:45:38 +0530

    minor change

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -33,7 +33,7 @@ public class DriverServiceImpl implements DriverService {
 		cab.setPerKmRate(10);
 		cab.setDriver(driver);
 		driver.setCab(cab);
-		cabRepository3.save(cab);
+//		cabRepository3.save(cab);
 		driverRepository3.save(driver);
 
 	}
